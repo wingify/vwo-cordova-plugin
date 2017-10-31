e45adf7736025b4a35efd34744297c7162b6b9d3
Amandeep Anguralla <amandeep.anguralla@wingify.com>
2017-10-31 15:41:41 +0530

    Code cleanup

--- www/plugin.js
+++ www/plugin.js
@@ -1,160 +1,264 @@
-
-var exec = require('cordova/exec');
-
-var PLUGIN_NAME = 'VWOCordovaPlugin';
-
-//noinspection JSUnusedGlobalSymbols
-
-var VWO = function () {};
-
-VWO.logLevelDebug   = 1
-VWO.logLevelInfo    = 2
-VWO.logLevelWarning = 3
-VWO.logLevelError   = 4
-VWO.logLevelOff     = 5
-
-/** Launch VWO Synchronously
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param level {number} Log level
+/**
  */
-VWO.setLogLevel = function(level){
-  if (typeof level !== 'number') {
-    throw new Error('Invalid log level');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'setLogLevel', [level]);
-};
 
-/** Launch VWO Synchronously
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param apiKey {string} The App Key generated in VWO's console
- * @param timeout {number} Connection Timeout of API
- */
-VWO.launchSynchronously = function(apiKey, timeout){
-  if (typeof timeout !== 'number') {
-    throw new Error('timeout must be a number');
-  }
-  if(!apiKey && typeof level !== 'string') {
-    throw new Error('Invalid API Key');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'launchSynchronously', [apiKey, timeout]);
-};
+import com.vwo.mobile.Initializer;
+import com.vwo.mobile.VWO;
+import com.vwo.mobile.VWOConfig;
+import com.vwo.mobile.events.VWOStatusListener;
+import com.vwo.mobile.listeners.ActivityLifecycleListener;
+import com.vwo.mobile.utils.VWOLog;
 
-/** Launch VWO Asynchronously
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param apiKey {string} The App Key generated in VWO's console
- */
-VWO.launch = function(apiKey){
-  if(!apiKey && typeof level !== 'string') {
-    throw new Error('Invalid API Key');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'launch', [apiKey]);
-};
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/** Launch VWO Asynchronously with Callback
- *
- * @param success {callback} Success Callback
- * @param failureCallback {callback} Failure Callback
- * @param apiKey {string} The App Key generated in VWO's console
- */
-VWO.launchWithCallback = function(apiKey, success, error){
-  if(!apiKey) {
-    throw new Error('Must pass in a API Key');
-  }
-  exec(success, error, PLUGIN_NAME, 'launchWithCallback', [apiKey]);
-};
+public class VWOCordovaPlugin extends CordovaPlugin {
 
-/** Get the Variation object for a key
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param key {string} Key for the campaign
- *
- * @return Variation object
- */
-VWO.variationForKey = function (key, success) {
-  if (!key) {
-    throw new Error('Must pass Key for Campaign');
-  }
-  exec( function(wrappedData) {
-    success(wrappedData[key]);
-  }, function(error) {}, PLUGIN_NAME, 'variationForKey', [key]);
-};
+    private VWOConfig mConfig;
+    private ActivityLifecycleListener listener;
 
-/** Get the Variation object for a key. Default value in case key is not found or null.
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param key {string} Key for the campaign
- * @param defaultValue {value} Default Valye
- * @return Variation object
- */
-VWO.variationForKeyWithDefaultValue = function(key, defaultValue, success){
-  var value = VWO.variationForKey(key, function(data) {
-      if (!data) {
-      success(defaultValue);
-    } else {
-      success(data);
+    private static final int LOG_LEVEL_DEBUG = 1;
+    private static final int LOG_LEVEL_INFO = 2;
+    private static final int LOG_LEVEL_WARNING = 3;
+    private static final int LOG_LEVEL_ERROR = 4;
+    private static final int LOG_LEVEL_OFF = 5;
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        listener = new ActivityLifecycleListener();
+        mConfig = new VWOConfig.Builder().setLifecycleListener(listener).build();
     }
-  });
-};
 
-/** Mark the conversion for goal
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param goal {string} Goal's name
- */
-VWO.markConversionForGoal = function (goal) {
-  if (!goal) {
-    throw new Error('Must pass Goal name');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'markConversionForGoal', [goal]);
-};
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        if (action.equals("launchSynchronously")) {
 
-/** Mark the conversion for goal with value
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param goal {string} Goal's name
- * @param value {double} Goal's value
- */
-VWO.markConversionForGoalWithValue = function (goal, value) {
-  if (typeof value !== 'number') {
-    throw new Error('Value must be a number');
-  }
-  if (!goal) {
-    throw new Error('Must pass Goal name');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'markConversionForGoalWithValue', [goal, value]);
-};
+            String apiKey = args.getString(0);
+            Double timeoutInSeconds = args.getDouble(1);
+            // Convert timeout to milliseconds
+            Long timeout = timeoutInSeconds.longValue();
+            launchSynchronously(apiKey, timeout, callbackContext);
+            return true;
 
-/** Set custom variable by passing key and value
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- * @param key {string} Key
- * @param value {string} Value
- */
-VWO.setCustomVariable = function(key, value){
-  if(!key || !value) {
-    throw new Error('Key or Value is null');
-  }
-  exec(function(data) {}, function(error) {}, PLUGIN_NAME, 'setCustomVariable', [key, value]);
-};
+        } else if (action.equals("launch")) {
 
-/** Get the version of the SDK
- *
- * @param success {callback} Success Callback
- * @param error {callback} Failure Callback
- */
-VWO.version = function(success) {
-  exec(success, function(error) {}, PLUGIN_NAME, 'version', []);
-};
+            String apiKey = args.getString(0);
+            launch(apiKey, callbackContext);
+            return true;
 
-module.exports = VWO;
+        } else if (action.equals("launchWithCallback")) {
+
+            String apiKey = args.getString(0);
+            launchWithCallback(apiKey, callbackContext);
+            return true;
+
+        } else if (action.equals("setCustomVariable")) {
+
+            String key = args.getString(0);
+            String value = args.getString(1);
+            setCustomVariable(key, value, callbackContext);
+            return true;
+
+        } else if (action.equals("version")) {
+
+            getVersion(callbackContext);
+            return true;
+
+        } else if (action.equals("variationForKey")) {
+
+            String key = args.getString(0);
+            getVariationForKey(key, callbackContext);
+            return true;
+
+        } else if (action.equals("markConversionForGoal")) {
+
+            String goalIdentifier = args.getString(0);
+            markConversionForGoal(goalIdentifier, callbackContext);
+            return true;
+
+        } else if (action.equals("markConversionForGoalWithValue")) {
+
+            String goalIdentifier = args.getString(0);
+            double value = Double.parseDouble(args.getString(1));
+            markConversionForGoalWithValue(goalIdentifier, value, callbackContext);
+            return true;
+
+        } else if (action.equals("setLogLevel")) {
+            int logLevel = args.getInt(0);
+            setLogLevel(logLevel, callbackContext);
+        }
+        return false;
+    }
+
+    private void setLogLevel(final int level, final CallbackContext callbackContext) {
+        final int logLevel;
+        switch (level) {
+            case LOG_LEVEL_DEBUG:
+                logLevel = VWOLog.CONFIG;
+                break;
+            case LOG_LEVEL_INFO:
+                logLevel = VWOLog.INFO;
+                break;
+            case LOG_LEVEL_WARNING:
+                logLevel = VWOLog.WARNING;
+                break;
+            case LOG_LEVEL_ERROR:
+                logLevel = VWOLog.SEVERE;
+                break;
+            case LOG_LEVEL_OFF:
+                logLevel = VWOLog.OFF;
+                break;
+            default:
+                VWOLog.e(VWOLog.CONFIG_LOGS, "Invalid log level : " + level, false, false);
+                logLevel = -1;
+        }
+
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                if(logLevel == -1) {
+                    callbackContext.error("Failed to change log level");
+                } else {
+                    VWOLog.setLogLevel(logLevel);
+                    callbackContext.success("Log level changed successfully");
+                }
+            }
+        });
+    }
+
+    private void markConversionForGoal(final String goalIdentifier, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                VWO.markConversionForGoal(goalIdentifier);
+            }
+        });
+    }
+
+    private void markConversionForGoalWithValue(final String goalIdentifier, final double value, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                VWO.markConversionForGoal(goalIdentifier, value);
+            }
+        });
+    }
+
+    private void getVariationForKey(final String key, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                JSONObject wrapperObject = new JSONObject();
+                Object object = VWO.getVariationForKey(key);
+
+                if (object == null) {
+                    try {
+                        wrapperObject.put(key, JSONObject.NULL);
+                        callbackContext.success(wrapperObject);
+                    } catch (JSONException exception) {
+                        VWOLog.e(VWOLog.DATA, exception, false, false);
+                    }
+                } else {
+                    try {
+                        wrapperObject.put(key, object);
+                        callbackContext.success(wrapperObject);
+                    } catch (JSONException exception) {
+                        VWOLog.e(VWOLog.DATA, exception, false, false);
+                    }
+                }
+            }
+        });
+    }
+
+    private void setCustomVariable(final String key, final String value, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                VWO.setCustomVariable(key, value);
+            }
+        });
+    }
+
+    private void launchSynchronously(final String apiKey, final Long timeout, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                VWO.with(cordova.getActivity(), apiKey).config(mConfig).launchSynchronously(timeout);
+                callbackContext.success("VWO Initialized");
+            }
+        });
+    }
+
+    private void getVersion(final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                callbackContext.success(VWO.version());
+            }
+        });
+    }
+
+    private void launch(final String apiKey, final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+
+                Initializer initializer = VWO.with(cordova.getActivity(), apiKey).config(mConfig);
+                if(callbackContext != null) {
+                    initializer.launch(new VWOStatusListener() {
+                        @Override
+                        public void onVWOLoaded() {
+                            callbackContext.success("VWO Initialized");
+                        }
+
+                        @Override
+                        public void onVWOLoadFailure(String reason) {
+                            callbackContext.error(reason);
+                        }
+                    });
+                } else {
+                    initializer.launch();
+                }
+            }
+        });
+    }
+
+    private void launchWithCallback(final String apiKey, final CallbackContext callbackContext) {
+
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                VWO.with(cordova.getActivity(), apiKey).config(mConfig).launch(new VWOStatusListener() {
+                    @Override
+                    public void onVWOLoaded() {
+                        callbackContext.success("VWO Loaded");
+                    }
+
+                    @Override
+                    public void onVWOLoadFailure(String reason) {
+                        callbackContext.error("VWO Load Failure");
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onPause(boolean multitasking) {
+        listener.onPause();
+        super.onPause(multitasking);
+    }
+
+    @Override
+    public void onResume(boolean multitasking) {
+        super.onResume(multitasking);
+        listener.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        listener.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        listener.onStart();
+    }
+}
